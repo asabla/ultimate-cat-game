f8b118b12a75da3378009bc011ebc7389678ec8a
sparven89@gmail.com <sparven89@gmail.com@04163fd1-8938-284e-e756-3796ab607322>
2011-04-19 13:48:38 +0000

    Uppdaterat! Fixat till så att highscore menyn nu kan nås

--- trunk/EnemyAtTheCats/src/katt/Menu.java
+++ trunk/EnemyAtTheCats/src/katt/Menu.java
@@ -11,78 +11,111 @@ import org.newdawn.slick.gui.MouseOverArea;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class Menu extends BasicGameState implements ComponentListener{
-	
+public class Menu extends BasicGameState implements ComponentListener
+{
 	private Image back = null;
 	private Image newgame = null;
 	private Image newgameOver = null;
+	private Image Highscore = null;
+	private Image HighscoreOver = null;
 	private MouseOverArea[] areas = new MouseOverArea[2];
 	private StateBasedGame game;
 	private int ID = -1;
-	
-	
-	public Menu(int ID){
+	private Database db;
+
+	public Menu(int ID)
+	{
 		super();
 		this.ID = ID;
+		db = new Database();
 	}
 
 	@Override
-	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		back.draw(0,0);
-		areas[0].render(container, g);
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException
+	{
+		back.draw(0, 0);
+		for(int i = 0; i < areas.length; i++)
+		{
+			areas[i].render(container, g);
+		}
 	}
 
 	@Override
-	public void componentActivated(AbstractComponent source) {
-		if(source == areas[0]){
+	public void componentActivated(AbstractComponent source)
+	{
+		if (source == areas[0])//Nytt spel
+		{
 			StateHandler.paused = false;
 			game.enterState(StateHandler.theGame);
 		}
+		if(source == areas[1])//Visa Highscore - INTE KLAR
+		{
+			Menu_Highscore hs = new Menu_Highscore(StateHandler.highscore);
+			hs.setHighscores(db.getHighscoreToArrayList(10, false));
+			
+			game.enterState(StateHandler.highscore);
+		}
 	}
 
 	@Override
-	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+	public void init(GameContainer container, StateBasedGame game) throws SlickException
+	{
 		this.game = game;
-		
+
 		back = new Image("data/Img/Back.png");
 		newgame = new Image("data/Img/Nyttspel1.png");
 		newgameOver = new Image("data/Img/Nyttspel2.png");
+		Highscore = new Image("data/Img/highscore1.png");
+		HighscoreOver = new Image("data/Img/highscore2.png");
 		
-		//container.setMouseCursor("data/Img/cursor.png", 0, 0);
-		
-		for(int i = 0; i<2; i++){
-			if(i == 0){
-			areas[i] = new MouseOverArea(container, newgame, 130, 100, 400, 52, this);
-			areas[i].setMouseOverImage(newgameOver);
+
+		// container.setMouseCursor("data/Img/cursor.png", 0, 0);
+
+		for (int i = 0; i < 2; i++)
+		{
+			if (i == 0)
+			{
+				areas[i] = new MouseOverArea(container, newgame, 130, 100, 400,
+						52, this);
+				areas[i].setMouseOverImage(newgameOver);
 			}
-			//if(i == 1){
-			//areas[i] = new MouseOverArea(container, newgame, 130, 100, 400, 52, this);
-			//areas[i].setMouseOverImage(newgameOver);
-			//}
+			if( i == 1)
+			{
+				areas[i] = new MouseOverArea(container, Highscore, 130, 200, 400, 52, this);
+				areas[i].setMouseOverImage(HighscoreOver);
+			}
+			
+			
+			// if(i == 1){
+			// areas[i] = new MouseOverArea(container, newgame, 130, 100, 400,
+			// 52, this);
+			// areas[i].setMouseOverImage(newgameOver);
+			// }
 		}
-		
+
 	}
 
 	@Override
-	public void update(GameContainer container,StateBasedGame game, int delta) throws SlickException {
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException
+	{
 		Input input = container.getInput();
-		
-		if (input.isKeyPressed(Input.KEY_ESCAPE)) {
+
+		if (input.isKeyPressed(Input.KEY_ESCAPE))
+		{
 			StateHandler.paused = true;
 			game.enterState(StateHandler.theGame);
 		}
 	}
 
 	@Override
-	public int getID() {
+	public int getID()
+	{
 		return ID;
 	}
-	
+
 	@Override
-	public void leave(GameContainer container, StateBasedGame game)
-			throws SlickException {
-		
-		
+	public void leave(GameContainer container, StateBasedGame game) throws SlickException
+	{
+
 	}
-	
 }
