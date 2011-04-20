c84ed5f957ce6b4e9d14b843fb802376ca771ce0
christian.werme <christian.werme@04163fd1-8938-284e-e756-3796ab607322>
2011-04-20 09:22:04 +0000

    Den är till emil för att vi jobbar med en grej. TextField och Font

--- EnemyAtTheCats/src/katt/GameOver.java
+++ EnemyAtTheCats/src/katt/GameOver.java
@@ -1,13 +1,18 @@
 package katt;
 
+import java.awt.Font;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
 import org.newdawn.slick.gui.AbstractComponent;
 import org.newdawn.slick.gui.ComponentListener;
 import org.newdawn.slick.gui.MouseOverArea;
+import org.newdawn.slick.gui.TextField;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.Game;
@@ -24,6 +29,9 @@ public class GameOver extends BasicGameState implements ComponentListener{
 	private Game ettGame;
 	private StateBasedGame game;
 	private int ID = -1;
+	private UnicodeFont myFont = getNewFont("Arial", 72);
+	private TextField field;
+	private String message;
 	
 	
 	
@@ -46,6 +54,15 @@ public class GameOver extends BasicGameState implements ComponentListener{
 		areas[1].render(container, g);
 		//game.getState(DenBraMenyn.theGame).render(container, game, g);
 		
+		
+		myFont.drawString(0,0,"test"); 
+		g.setFont(myFont);
+		g.drawString("Fisk", 100, 100);
+		field.render(container, g);
+//		field.setText("@ŠšŠ!");
+		field.setFocus(true);
+		field.setMaxLength(50);
+		field.setAcceptingInput(true);
 		
 	}
 
@@ -91,13 +108,23 @@ public class GameOver extends BasicGameState implements ComponentListener{
 		}
 		
 		
+		field = new TextField(container, myFont, 130, 300, 300, 72, new ComponentListener()
+		{
+			public void componentActivated(AbstractComponent source)
+			{
+				message = "borg";
+				field.setFocus(true);
+			}
+		});
+		
+		
 		
 	}
 
 	@Override
 	public void update(GameContainer container,StateBasedGame game, int delta) throws SlickException {
 		
-			
+		myFont.loadGlyphs(1);
 	}
 
 
@@ -112,5 +139,12 @@ public class GameOver extends BasicGameState implements ComponentListener{
 			throws SlickException {
 		
 	}
+	
+	public UnicodeFont getNewFont(String fontName, int fontSize) { 
+	      UnicodeFont font = new UnicodeFont(new Font(fontName, Font.PLAIN, fontSize)); 
+	      font.getEffects().add(new ColorEffect(java.awt.Color.white)); 
+//	      font.getEffects().add(new ShadowEffect(java.awt.Color.black, 5, 5, 0.5f)); 
+	      return (font); 
+	   } 
 	
 }
