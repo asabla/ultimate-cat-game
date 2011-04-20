0561b4142f57a8ded50cd2fb727e1351afa4e583
christian.werme <christian.werme@04163fd1-8938-284e-e756-3796ab607322>
2011-04-20 08:08:06 +0000

    La till en knapp så att man kan gå tillbaks till huvudmenyn.

--- EnemyAtTheCats/src/katt/GameOver.java
+++ EnemyAtTheCats/src/katt/GameOver.java
@@ -17,12 +17,14 @@ public class GameOver extends BasicGameState implements ComponentListener{
 	
 	private Image back = null;
 	private Image newgame = null;
-	private Image highscore = null;
+	private Image newgameOver = null;
+	private Image backOver = null;
 	private MouseOverArea[] areas = new MouseOverArea[2];
 	private GameContainer gameContainer;
 	private Game ettGame;
 	private StateBasedGame game;
 	private int ID = -1;
+	
 	
 	
 	public GameOver(int ID){
@@ -32,11 +34,16 @@ public class GameOver extends BasicGameState implements ComponentListener{
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		g.drawString("Game Over", 30, 30);
-		g.setBackground(Color.red);
+		
+		g.drawString("Game Over", 300, 50);
+		g.setBackground(Color.magenta);
+		
+		
+		
 		
 		
 		areas[0].render(container, g);
+		areas[1].render(container, g);
 		//game.getState(DenBraMenyn.theGame).render(container, game, g);
 		
 		
@@ -48,16 +55,24 @@ public class GameOver extends BasicGameState implements ComponentListener{
 			StateHandler.paused = false;
 			game.enterState(StateHandler.theGame);
 			}
+		if(source == areas[1])
+		{
+			StateHandler.paused = false;
+			game.enterState(StateHandler.menu);
+		}
 		
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
 		this.game = game;
 		
 		back = new Image("data/Img/Back.png");
-		newgame = new Image("data/Img/Nytt.png");
-		highscore = new Image("data/Img/Nyttover.png");
+		newgame = new Image("data/Img/nyttspel1.png");
+		newgameOver = new Image("data/Img/nyttspel2.png");
+		back = new Image("data/Img/tillbaka1.png");
+		backOver = new Image("data/Img/tillbaka2.png");
 		
 		//container.setMouseCursor("data/Img/cursor.png", 0, 0);
 		
@@ -65,10 +80,17 @@ public class GameOver extends BasicGameState implements ComponentListener{
 		{
 			if(i == 0)
 			{
-				areas[i] = new MouseOverArea(container, newgame, 250, 100, 225, 52, this);
-				areas[i].setMouseOverImage(highscore);
+				areas[i] = new MouseOverArea(container, newgame, 130, 100, 400, 52, this);
+				areas[i].setMouseOverImage(newgameOver);
+			}
+			if(i == 1)
+			{
+				areas[i] = new MouseOverArea(container, back, 135, 200, 400, 52, this);
+				areas[i].setMouseOverImage(backOver);
 			}
 		}
+		
+		
 		
 	}
 
