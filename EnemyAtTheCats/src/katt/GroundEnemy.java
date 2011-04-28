37bdc03ef397c54952bf88fd5c7b1300237f3431
mange.johanson <mange.johanson@04163fd1-8938-284e-e756-3796ab607322>
2011-04-28 10:55:05 +0000

    Animation av GroundEnemy

--- EnemyAtTheCats/src/katt/GroundEnemy.java
+++ EnemyAtTheCats/src/katt/GroundEnemy.java
@@ -1,7 +1,9 @@
 package katt;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.geom.Rectangle;
 
 /*
@@ -10,12 +12,16 @@ import org.newdawn.slick.geom.Rectangle;
 public class GroundEnemy extends EnemyObject {
 	// Int = typen av fiende
 	private int type;
+	private Animation stand;
+	private SpriteSheet sheet;
+	private Animation currentAnimation;
 
 	public GroundEnemy(int type) {
 		super();
 		this.type = type;
-		setImgLoc("data/Img/GroundEnemy" + type + ".png");
-
+		setImgLoc("data/Img/groundEnemy2.png");
+		this.stand = createAnimation("data/Img/groundEnemy2.png", 70, 58, 10, 65,0);
+		this.currentAnimation = stand;
 		try {
 			// Objektets bild som används för att sätta storleken på rektangeln
 			Image i = new Image(getImgLoc());
@@ -31,6 +37,47 @@ public class GroundEnemy extends EnemyObject {
 	public int getType() {
 		return type;
 	}
+	private Animation createAnimation(String fileDirectory, int sSizeX, 
+			int sSizeY, int frameCount, float speed, int animationYPos) {
+		
+		return addFramesToAnimation(createSheet(fileDirectory, sSizeX, sSizeY), frameCount, speed, animationYPos);
+	}
+	private SpriteSheet createSheet(String fileDir, int frameWidth, int frameHeight){
+		// load sprite sheet and put in list
+		try {
+			return sheet = new SpriteSheet(fileDir, frameWidth, frameHeight);
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
+	private Animation addFramesToAnimation(SpriteSheet sht, int frameCount, float speed, int animationYPos){
+		Animation tempAnim = new Animation();
+		for (int frame = 0; frame < frameCount; frame++) {
+			tempAnim.addFrame(sht.getSprite(animationYPos, frame), (int) speed);
+		}
+		return tempAnim;
+		
+	}
+	public SpriteSheet getSheet() {
+		return sheet;
+	}
+	public Animation getCurrentAnimation() {
+		return currentAnimation;
+	}
+	public void setCurrentAnimation(Animation currentAnimation) {
+		this.currentAnimation = currentAnimation;
+	}
+	public void updateAnimationSpeed() {
+		this.currentAnimation.setSpeed(TheGame.gameSpeed);
+	}
+	
+	
+	
+	
+	
+	
 
 	// Skapar en ny slumpmässigt vald position för objektet
 	@Override
