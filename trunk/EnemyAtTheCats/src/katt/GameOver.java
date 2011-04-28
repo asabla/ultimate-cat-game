bf82480457cb10f0e0b70d9ade1578eadd0d21db
christian.werme <christian.werme@04163fd1-8938-284e-e756-3796ab607322>
2011-04-28 10:38:01 +0000

    gameoverhea

--- trunk/EnemyAtTheCats/src/katt/GameOver.java
+++ trunk/EnemyAtTheCats/src/katt/GameOver.java
@@ -31,13 +31,9 @@ public class GameOver extends BasicGameState implements ComponentListener {
 	private StateBasedGame game;
 	private int ID = -1;
 	private UnicodeFont myFont;
-	private TextField f_namn;
-	private TextField f_email1;
-	private TextField f_email2;
-	private TextField f_telefon;
-	private TextField f_points;
-	private String message = "google";
-	private String snabel = "@";
+	private String f_namn;
+	private String f_email;
+	private String f_telefon;
 	private static String pscore;
 	private Image sendScore;
 	private Database db;
@@ -45,6 +41,8 @@ public class GameOver extends BasicGameState implements ComponentListener {
 	private Highscores scores;
 	private String superName;
 	private Score ss;
+	
+	private StartScreen start;
 
 	public GameOver(int ID) {
 		super();
@@ -63,21 +61,18 @@ public class GameOver extends BasicGameState implements ComponentListener {
 		areas[1].render(container, g);
 		areas[2].render(container, g);
 
-		// myFont.drawString(0,0,"test"); //GÂr att anv‰nda fonten till att
-		// skriva ut
-
-		f_namn.render(container, g);
-		f_email1.render(container, g);
-		f_email2.render(container, g);
-		f_telefon.render(container, g);
-//		f_points.render(container, g);
-
+		f_namn = start.getNamn();
+		f_email = start.getMail();
+		f_telefon = start.getTel();
+		
+		
 		g.drawString(superName, 130, 70);
 		g.setFont(myFont);
-		g.drawString(message, 200, 550);
+		g.drawString(pscore, 150, 350);
 		
-		g.drawString(snabel, 355, 310);
-		g.drawString(pscore, 150, 390);
+		g.drawString(f_namn, 150, 260);
+		g.drawString(f_email, 150, 290);
+		g.drawString(f_telefon, 150, 320);
 		
 		g.drawImage(sadCat, 590, 410);
 		
@@ -104,31 +99,7 @@ public class GameOver extends BasicGameState implements ComponentListener {
 			game.enterState(StateHandler.MENU);
 
 		}
-		if (source == areas[2]) {
-			StateHandler.soundBank.playSound("boing");
-			int tmp = Integer.parseInt(pscore); // Parsar om texten
-			String mail = f_email1.getText() + "@" + f_email2.getText();												// ifrÂn highscore
-															// till en int
-			db.sendHighscore(f_namn.getText(), tmp, mail,
-					f_telefon.getText()); // Testar att skicka in resultatet
-		}
-
-		if (source == f_namn) {
-			StateHandler.soundBank.playSound("boing");
-			f_email1.setFocus(true);
-		}
-		if (source == f_email1) {
-			StateHandler.soundBank.playSound("boing");
-			f_email2.setFocus(true);
-		}
-		if(source == f_email2);{
-			StateHandler.soundBank.playSound("boing");
-			f_telefon.setFocus(true);
-		}
-		if (source == f_telefon) {
-			StateHandler.soundBank.playSound("boing");
-			// setPoints
-		}
+		
 	}
 
 	@SuppressWarnings("deprecation")
@@ -166,6 +137,8 @@ public class GameOver extends BasicGameState implements ComponentListener {
 		}
 
 		myFont = getNewFont("Arial", 24);
+		
+		start = new StartScreen(null);
 	}
 
 	@Override
@@ -186,11 +159,7 @@ public class GameOver extends BasicGameState implements ComponentListener {
 
 	public void enter(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		f_namn = new TextField(container, myFont, 150, 270, 200, 35);
-		f_email1 = new TextField(container, myFont, 150, 310, 200, 35);
-		f_email2 = new TextField(container, myFont, 385, 310, 200, 35);
-		f_telefon = new TextField(container, myFont, 150, 350, 200, 35);
-//		f_points = new TextField(container, myFont, 150, 390, 200, 35);
+		
 		db = new Database();
 		func = new Functions();
 		scores = new Highscores();
@@ -198,13 +167,6 @@ public class GameOver extends BasicGameState implements ComponentListener {
 		ss.setName(func.getPlayerName());
 		superName = scores.check(ss); //Strängen används för att skriva ut info till spelaren
 
-		f_namn.setText(func.getPlayerName()); // S‰tter spelarens namn fˆrdefinerat
-		f_email1.setText("mail"); // S‰tter spelarens email
-		f_email2.setText("gmail.com");//Sätter vad som ska stå efter @
-		f_telefon.setText("324-234234"); // S‰tter spelarens telefon
-//		f_points.setText(GameOver.pscore); // H‰mtar spelarens po‰ng n‰r denne
-										   // fˆrlorar
-//		f_namn.setFocus(true);
 	}
 
 	@Override
@@ -217,7 +179,7 @@ public class GameOver extends BasicGameState implements ComponentListener {
 		UnicodeFont font = new UnicodeFont(new Font(fontName, Font.PLAIN,
 				fontSize));
 		font.addGlyphs("@");
-		font.getEffects().add(new ColorEffect(java.awt.Color.white)); // MÂste
+		font.getEffects().add(new ColorEffect(java.awt.Color.black)); // MÂste
 																		// vara
 																		// minst
 																		// en
